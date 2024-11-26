fcff13a6d524c75f0e4927a455d928bfea9ad420
FrostScythe <71485558+JaredSaenz@users.noreply.github.com>
2024-11-26 09:55:00 -0600

    Index update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import express from "express";
-import { config } from 'dotenv';
 import path from 'path';
 import morgan from "morgan";
-import pg from 'pg';
-
-config();
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.json());
-const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-});
 
 const __dirname = path.resolve(); 
 app.use(express.static(path.join(__dirname)));
